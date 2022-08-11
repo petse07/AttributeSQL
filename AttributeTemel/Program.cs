41158e6a907802d4aa7d27da66131a638ff709d8
petse07 <gozcucf@gmail.com>
2022-08-11 15:29:09 +0300

    sql tablo yaratma için kullanılan "insert into...." sorguyu oluşturduk.

--- AttributeTemel/Program.cs
+++ AttributeTemel/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using AttributeTemel;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+UrunEntity urn = new UrunEntity();
+urn.UrunAdi = "Pentium CPU";
+urn.Fiyat = 90;
+urn.SonSatisTarihi = DateTime.Now.AddDays(30);
+urn.Insert();
+
+
+
+
